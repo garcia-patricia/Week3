ba032514ba6aca4a5697dd2fdc9bfa4b954038cb
Garcia <p_garcia3@tss-hr420-dm39.local>
2018-05-30 14:42:59 -0400

    added basic js

--- js/main.js
+++ js/main.js
@@ -12,16 +12,40 @@ comment
 
  */
 
-var words = "This is a string";
-var num = 1;
-console.log(words);
-console.log("Your lucky number is "+num);
-var teams = ["Knights" ,"Caps" ,"Leafs"]; 
-console.log(teams[0]); // 1st position of an array is 0
+// var words = "This is a string";
+// var num = 1;
+// console.log(words);
+// console.log("Your lucky number is "+num);
+// var teams = ["Knights" ,"Caps" ,"Leafs"]; 
+// console.log(teams[0]); // 1st position of an array is 0
 
-function i_am_a_function(){
-	console.log("From i_am_a_function()");
+	
+	var iconSet = document.querySelectorAll(".icons");
+	console.log(iconSet);
+
+function logLoaded(evt) {
+	// console.log("svg loaded");
+	// console.log(evt);
+	var targetSVG = evt.currentTarget.contentDocument;
+	// console.log(targetSVG);
+	var theIcon = targetSVG.querySelector(".mainIcon");
+	// console.log(theIcon);
+	theIcon.addEventListener("click",logClicked, false);
 }
 
-window.addEventListener("load",i_am_a_function,false);
+function logClicked(evt) {
+	console.log(evt.currentTarget.id);
+}
+// function i_am_a_function(){
+// 	console.log("From i_am_a_function()");
+// }
+
+// window.addEventListener("load",i_am_a_function,false);
+
+for(var i=0; i<iconSet.length; i++) {
+	// console.log(i);
+	iconSet[i].addEventListener("load",logLoaded, false);
+}
+
 }) ();
+
